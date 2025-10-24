fb3172988af22d146c480fbba55b21b0a16348c8
gardenswartzj <110622967+gardenswartzj@users.noreply.github.com>
2025-10-23 20:55:23 -0400

    Update userscript.js

---
+++ userscript.js
@@ -0,0 +1,8 @@
+// Import the TizenPrime module code
+import tprimeMod from './tprime-mod.js';
+
+// Execute the TFlix module
+tprimeMod();
+
+// Import any CSS files if needed
+// import './ui.css';
